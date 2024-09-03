1f0b1ecd5842b380195282afd8ebfc330ff5b5df
nelsonfjunior <ferreiranelson123@hotmail.com>
2024-09-03 11:53:47 -0300

    feat: create http methods

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -3,11 +3,12 @@ import { TaskService } from '../../services/task.service';
 import { Tarefa } from '../../../Tarefa';
 import { CommonModule } from '@angular/common';
 import { TaskItemComponent } from "../task-item/task-item.component";
+import { AddTaskComponent } from "../add-task/add-task.component";
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
-  imports: [CommonModule, TaskItemComponent],
+  imports: [CommonModule, TaskItemComponent, AddTaskComponent],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css'
 })
@@ -24,5 +25,20 @@ export class TasksComponent implements OnInit {
     })
   }
 
+  deleteTask(tarefa: Tarefa){
+    this.taskService.deleteTask(tarefa).subscribe(() =>
+      (this.tarefas = this.tarefas.filter((t) => t.id == tarefa.id)));
+  }
+
+  toggleConcluido(tarefa: Tarefa){
+    tarefa.concluido = !tarefa.concluido;
+    this.taskService.updateTask(tarefa).subscribe();
+  }
+
+  addTask(tarefa: Tarefa){
+    this.taskService.addTask(tarefa).subscribe((tarefa) => {
+      this.tarefas.push(tarefa);
+    });
+  }
 
 }
--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,3 +1,4 @@
+
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
@@ -14,5 +15,17 @@ export class TaskService {
 
   getTasks() : Observable<Tarefa[]>{
     return this.http.get<Tarefa[]>(this.apiUrl);
+  }
+
+  deleteTask(tarefa: Tarefa): Observable<Tarefa>{
+    return this.http.delete<Tarefa>(`${this.apiUrl}/${tarefa.id}`);
+  }
+
+  updateTask(tarefa: Tarefa): Observable<Tarefa>{
+    return this.http.put<Tarefa>(`${this.apiUrl}/${tarefa.id}`, tarefa);
+  }
+
+  addTask(tarefa: Tarefa): Observable<Tarefa>{
+    return this.http.post<Tarefa>(`${this.apiUrl}`, tarefa);
   }
 }
